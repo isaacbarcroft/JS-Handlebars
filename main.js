5c8db574a6390aea85e0c36d2b6a20c9971fa2f3
isaacbarcroft <i_barcroft@yahoo.com>
2021-09-22 21:49:22 -0400

    avatar/login

---
+++ main.js
@@ -0,0 +1,52 @@
+      
+
+
+    //     // const API_KEY ='c3e891b7e7b32db73c2d';
+    //     const BASE_URL = 'https://api.github.com/users/isaacbarcroft';
+    //     // const USER = 'isaacbarcroft'
+    
+    //     const generateHTML = (data) => {
+    //             console.log(document.querySelector('.dog'))
+    //             const source = document.querySelector('#avatar').innerHTML
+    //             const template = Handlebars.compile(source);
+    //             const context = data; 
+    //             const html = template(context); 
+    //             document.querySelector('.dog').innerHTML = html;
+    //         }
+            
+    //         fetch(`${BASE_URL}`)
+    //         .then(response => response.json())
+    //         .then(data => generateHTML(data))
+            
+    //     })();
+
+    //     generateHTML
+
+        // const API_KEY ='c3e891b7e7b32db73c2d'
+
+ (function (){
+
+    // const generateHTML = (data, read, html) => {
+    //     const rawTemplate = document.querySelector(read).innerHTML;
+    //     const compiledTemplateFunc= Handlebars.compile(rawTemplate);
+    //     const ourGeneratedHTML = compiledTemplateFunc(data);
+    //     document.querySelector(html).innerHTML = ourGeneratedHTML;
+    // }
+        const BASE_URL = 'https://api.github.com/users/isaacbarcroft';
+        // const USER = 'isaacbarcroft'
+
+        const generateHTML = (data) => {
+                const source = document.querySelector('#avatar').innerHTML
+                const template = Handlebars.compile(source);
+                const context = data; 
+                const html = template(context); 
+                document.querySelector('.dog').innerHTML = html;
+                document.querySelector('.portrait').src = data.avatar_url
+                document.querySelector('.name').innerHTML= {...data}.login
+            }
+        fetch(`${BASE_URL}`)
+            .then (response => response.json())
+            .then(data => generateHTML(data))
+           
+        
+        })();
